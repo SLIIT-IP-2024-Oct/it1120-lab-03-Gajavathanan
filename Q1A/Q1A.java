a836420d5722821413041b412f99440f9a2310a8
Gajavathanan <gajangajavathanan@gmail.com>
2024-11-08 12:37:26 +0530

    Add files via upload

---
+++ Q1A/Q1A.java
@@ -0,0 +1,16 @@
+import java. until. Scanner;
+public class IT24104065Lab3Q1A {
+       public static void main (String[] args) {
+       Scanner input= new Scanner (System.in);
+       System. out. print("Enter the price of one 1kg of rice;");
+       double Price_Per_KG =input.nextDouble();
+       System.out.print("Enter the price of kilograms you want to buy:");
+       double Number_Of_KG = input.nextDouble(); 
+       double TotalAmount =Prince_Per_KG * NUMBER_Of_KG;
+       System.out.println("The total amount is:" + TotalAmount);
+       
+}
+
+}
+
+ 
